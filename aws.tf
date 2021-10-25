74f90a5a4b06020df11eb8ccaefa0b132eba7c6c
profiles <dmitry@mail.ru>
2021-10-25 14:12:00 +0300

    Stable fix 1

--- aws.tf
+++ aws.tf
@@ -6,15 +6,13 @@ terraform {
     }
   }
 }
+
 provider "aws" {
   region = "eu-central-1"
 }
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC2Z5nPIvrkgu/r9W8JcqkTlZqXFyIlA09biF+qGjQ9yO3quw8WzfyWFOJySg6O4BOJY6O0e48XVl/gH/cU4Rdcj+C2tiiYHQmV7NLljsAn5d4SCNhZBnTVh4n84U1mwzeCZRWV69fZ6K9+5cRBsncb9ut5+OrEpm/mc43OGeVD170p1a7GjXyc+UGUDlYRRaWUJPNqWq/LA9ZlOpMlF2meyZ2p0W9m3mx9RrHbATUzpoZ58/vC663GWHYQpek4l/rvm0v+/1qKxCXMXF3jsCAN2XC3h9WlDPsecT+UD1RogVu1Et6A+9LaVx2thPa7LkosIp3fIdAd+HGI/oL5rKWPpIHjvD4abHKOhAwwgkHwU+xAw+e16mHxCTFqTOaMIa+63avb+jN3SCZkBLqCD3uuArwL09TqcKnmH7TWZb3dnrgfXlDlDz863TRgtmc3gawGqZKTGlAAN1hH9tSGvJRwNQnLzvnkrTFdspEFQVGwWLTl8RO+SqhfUOK2J3th1h8= profiles@dell"
-}
+
 resource "aws_instance" "build" {
-  key_name = "${aws_key_pair.deployer.key_name}"
+  key_name = "${var.public_key}"
   ami           = "ami-05f7491af5eef733a"
   instance_type = "t2.micro"
   user_data = <<EOF
@@ -28,6 +26,7 @@ export AWS_DEFAULT_REGION=eu-central-1
 aws s3 cp /boxfuse-sample-java-war-hello/target/hello-1.0.war s3://mybacket1.dmitry-test.com
 EOF
 }
+
 resource "aws_instance" "prod" {
   key_name = "${aws_key_pair.deployer.key_name}"
   ami           = "ami-05f7491af5eef733a"
